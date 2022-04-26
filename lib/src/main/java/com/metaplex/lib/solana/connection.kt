aed720a1b31148c730f5fb0619c921ac318d5347
Arturo Jamaica <me@arturojamaica.com>
2022-04-27 01:32:33 +0900

    Add Solana connection

---
+++ lib/src/main/java/com/metaplex/lib/solana/connection.kt
@@ -0,0 +1,53 @@
+package com.metaplex.lib.solana
+
+import com.solana.api.Api
+import com.solana.api.getAccountInfo
+import com.solana.api.getMultipleAccounts
+import com.solana.api.getProgramAccounts
+import com.solana.core.PublicKey
+import com.solana.models.ProgramAccount
+import com.solana.models.buffer.BufferInfo
+import com.solana.networking.NetworkingRouter
+import com.solana.networking.RPCEndpoint
+import com.solana.vendor.borshj.BorshCodable
+
+interface Connection {
+    fun <T: BorshCodable> getProgramAccounts(account: PublicKey,
+                                                 decodeTo: Class<T>,
+                                                 onComplete: (Result<List<ProgramAccount<T>>>) -> Unit
+    )
+    fun <T: BorshCodable> getAccountInfo(account: PublicKey,
+                              decodeTo: Class<T>,
+                              onComplete: ((Result<BufferInfo<T>>) -> Unit))
+
+    fun <T: BorshCodable> getMultipleAccountsInfo(
+        accounts: List<PublicKey>,
+        decodeTo: Class<T>,
+        onComplete: ((Result<List<BufferInfo<T>>>) -> Unit)
+    )
+}
+
+class SolanaConnectionDriver(endpoint: RPCEndpoint): Connection {
+    val solanaRPC: Api = Api(NetworkingRouter(endpoint))
+
+    override fun <T: BorshCodable> getProgramAccounts(account: PublicKey,
+                                                      decodeTo: Class<T>,
+                                                      onComplete: (Result<List<ProgramAccount<T>>>) -> Unit
+    ){
+        solanaRPC.getProgramAccounts(account, decodeTo, onComplete)
+    }
+
+    override fun <T: BorshCodable> getAccountInfo(account: PublicKey,
+                                    decodeTo: Class<T>,
+                                    onComplete: ((Result<BufferInfo<T>>) -> Unit)){
+        solanaRPC.getAccountInfo(account, decodeTo, onComplete)
+    }
+
+    fun <T: BorshCodable> getMultipleAccountsInfo(
+        accounts: List<PublicKey>,
+        decodeTo: Class<T>,
+        onComplete: ((Result<List<BufferInfo<T>>>) -> Unit)
+    ){
+        solanaRPC.getMultipleAccounts(accounts, decodeTo, onComplete)
+    }
+}
