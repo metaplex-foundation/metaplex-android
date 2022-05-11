074969063e51dd93548a34e7798d4f00032dac2b
Arturo Jamaica <me@arturojamaica.com>
2022-05-11 17:36:59 -0500

    Tests find program address

---
+++ lib/src/test/java/com/metaplex/lib/programs/token_metadata/accounts/MetadataAccountTests.kt
@@ -0,0 +1,29 @@
+package com.metaplex.lib.programs.token_metadata.accounts
+
+import com.metaplex.lib.modules.nfts.models.MetaplexContstants
+import com.metaplex.lib.programs.token_metadata.MasterEditionAccount
+import com.metaplex.lib.programs.token_metadata.MetadataAccount
+import com.solana.core.PublicKey
+import org.bitcoinj.core.Base58
+import org.junit.Assert
+import org.junit.Test
+
+class MetadataAccountTests {
+    @Test
+    fun testFindAddress() {
+        val mintKey = PublicKey("HG2gLyDxmYGUfNWnvf81bJQj38twnF2aQivpkxficJbn")
+        val pdaSeeds = listOf(
+            MetaplexContstants.METADATA_NAME.toByteArray(),
+            Base58.decode(MetaplexContstants.METADATA_ACCOUNT_PUBKEY),
+            mintKey.toByteArray()
+        )
+
+        val expectedPdaAddress = PublicKey.findProgramAddress(
+            pdaSeeds,
+            PublicKey(MetaplexContstants.METADATA_ACCOUNT_PUBKEY)
+        )
+
+        val pda = MetadataAccount.pda(mintKey).getOrThrows()
+        Assert.assertEquals(expectedPdaAddress.address.toBase58(), pda.toBase58())
+    }
+}
