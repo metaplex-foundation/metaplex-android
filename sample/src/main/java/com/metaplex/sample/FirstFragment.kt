5b22a6f0c3e70b58aba70967b71d4601abd977a9
thedevyansh <schawla333333@gmail.com>
2022-06-27 01:37:34 +0530

    Modified formatting values and added a layout for showing NFT details

--- sample/src/main/java/com/metaplex/sample/FirstFragment.kt
+++ sample/src/main/java/com/metaplex/sample/FirstFragment.kt
@@ -1,6 +1,6 @@
 package com.metaplex.sample
 
-import  android.content.Context
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
--- sample/src/main/java/com/metaplex/sample/NftDetailsActivity.kt
+++ sample/src/main/java/com/metaplex/sample/NftDetailsActivity.kt
@@ -2,10 +2,21 @@ package com.metaplex.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.TextView
+import com.metaplex.lib.Metaplex
+import com.metaplex.lib.drivers.indenty.ReadOnlyIdentityDriver
+import com.metaplex.lib.drivers.storage.OkHttpSharedStorageDriver
 import com.metaplex.lib.modules.nfts.models.NFT
+import com.metaplex.lib.solana.SolanaConnectionDriver
+import com.solana.core.PublicKey
+import com.solana.networking.RPCEndpoint
 
 class NftDetailsActivity : AppCompatActivity() {
+    private lateinit var metaplex: Metaplex
+    private val ownerPublicKey = PublicKey("CN87nZuhnFdz74S9zn3bxCcd5ZxW55nwvgAv5C2Tz3K7")
+
     private lateinit var mintAccountTextView : TextView
 
     companion object {
@@ -18,8 +29,29 @@ class NftDetailsActivity : AppCompatActivity() {
 
         mintAccountTextView = findViewById(R.id.mintAccountTextView)
 
+        val solanaConnection = SolanaConnectionDriver(RPCEndpoint.mainnetBetaSolana)
+        val solanaIdentityDriver = ReadOnlyIdentityDriver(ownerPublicKey, solanaConnection.solanaRPC)
+        val storageDriver = OkHttpSharedStorageDriver()
+
+        metaplex = Metaplex(solanaConnection, solanaIdentityDriver, storageDriver)
+
         val mintAccount = intent?.extras?.getString(MINT_ACCOUNT).toString()
 
-        mintAccountTextView.text = getString(R.string.mint_account, mintAccount)
+        metaplex.nft.findNftByMint(PublicKey(mintAccount)) { result ->
+            result.onSuccess { nft ->
+                fetchOffChainMetadata(nft)
+            }
+        }
+    }
+
+    private fun fetchOffChainMetadata(nft : NFT) {
+        nft.metadata(metaplex) { result ->
+            result.onSuccess {
+                // Don't Use this change of thread hack. This is a over simplify example.
+                Handler(Looper.getMainLooper()).post(Runnable {
+                    mintAccountTextView.text = it.description
+                })
+            }
+        }
     }
 }
