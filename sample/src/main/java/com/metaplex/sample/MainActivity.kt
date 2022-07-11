a4190543f675e3c1dfd2bf6670f9e5e1a365b815
Marco Martinez <funkatronicsmail@gmail.com>
2022-07-11 11:40:54 -0600

    martini/DX-72: Add Lint Action (#43)

--- sample/src/main/java/com/metaplex/sample/MainActivity.kt
+++ sample/src/main/java/com/metaplex/sample/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.metaplex.sample
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.metaplex.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
