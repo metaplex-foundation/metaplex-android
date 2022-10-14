2468c6deab678342b8fbc31ccd98804a70e0edb9
funkatronics <funkatronicsmail@gmail.com>
2022-10-14 09:08:14 -0600

    fix find nft by owner test

--- lib/src/test/java/com/metaplex/lib/modules/nfts/operations/FindNftsByOwnerOnChainOperationHandlerTests.kt
+++ lib/src/test/java/com/metaplex/lib/modules/nfts/operations/FindNftsByOwnerOnChainOperationHandlerTests.kt
@@ -3,36 +3,50 @@
 package com.metaplex.lib.modules.nfts.operations
 
 import com.metaplex.lib.Metaplex
-import com.metaplex.lib.MetaplexTestUtils
-import com.metaplex.lib.MetaplexTestUtils.TEST_ACCOUNT_PUBLICKEY
-import com.metaplex.lib.generateMetaplexInstance
+import com.metaplex.lib.drivers.indenty.KeypairIdentityDriver
+import com.metaplex.lib.drivers.rpc.JdkRpcDriver
+import com.metaplex.lib.drivers.solana.Commitment
+import com.metaplex.lib.drivers.solana.SolanaConnectionDriver
+import com.metaplex.lib.drivers.solana.TransactionOptions
+import com.metaplex.lib.drivers.storage.MemoryStorageDriver
+import com.metaplex.lib.modules.nfts.models.Metadata
 import com.metaplex.lib.modules.nfts.models.NFT
+import com.solana.core.Account
+import com.util.airdrop
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
+import java.net.URL
 
 class FindNftsByOwnerOnChainOperationHandlerTests {
-
-    val metaplex: Metaplex get() = MetaplexTestUtils.generateMetaplexInstance()
 
     @Test
     fun testFindNftsByOwnerOnChainOperation() = runTest {
         // given
-        val accountKey = TEST_ACCOUNT_PUBLICKEY
-        val expectedNftMintKey = "71PdnsexRQG92ZcSpEV8nn5XFqPzfK5j86yUWRF5NLyp"
-        val expectedNftAuthKey = "AVgijgTG4WKDycFwYhqioMreXJpz14no8dbE8EF5roZV"
+        val rpcUrl = URL("http://127.0.0.1:8899")
+        val owner = Account()
+        val connection = SolanaConnectionDriver(JdkRpcDriver(rpcUrl),
+            transactionOptions = TransactionOptions(Commitment.CONFIRMED, skipPreflight = true)
+        )
+        val metaplex = Metaplex(connection,
+            KeypairIdentityDriver(owner, connection), MemoryStorageDriver())
 
-        //when
-        val resultList: List<NFT?>? = FindNftsByOwnerOnChainOperationHandler(metaplex)
-            .handle(accountKey).getOrNull()
+        // when
+        connection.airdrop(owner.publicKey, 1f)
+        val createdNft = metaplex.nft.create(Metadata("My NFT",
+            uri = "http://example.com/1234abcd", sellerFeeBasisPoints = 250)).getOrThrow()
 
-        val actualNftAuthKey = resultList?.find {
-            it?.metadataAccount?.mint?.toBase58() == expectedNftMintKey
-        }?.metadataAccount?.update_authority?.toBase58()
+        val ownerNfts: List<NFT?> = FindNftsByOwnerOnChainOperationHandler(metaplex)
+            .handle(owner.publicKey).getOrThrow()
 
         // then
-        Assert.assertNotNull(resultList)
-        Assert.assertEquals(expectedNftAuthKey, actualNftAuthKey)
+        Assert.assertNotNull(ownerNfts.first())
+        Assert.assertTrue(ownerNfts.size == 1)
+        Assert.assertEquals(createdNft.mint, ownerNfts.first()?.mint)
+        Assert.assertEquals(createdNft.metadataAccount.mint,
+            ownerNfts.first()?.metadataAccount?.mint)
+        Assert.assertEquals(createdNft.metadataAccount.update_authority,
+            ownerNfts.first()?.metadataAccount?.update_authority)
     }
 }
