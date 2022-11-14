c4c9c98a104449926e142ff11762daa471f8f4b9
funkatronics <funkatronicsmail@gmail.com>
2022-11-14 10:37:37 -0700

    fix find by cm logic, fix some unit tests

--- lib/src/main/java/com/metaplex/lib/modules/nfts/operations/FindNftsByCandyMachineOnChainOperationHandler.kt
+++ lib/src/main/java/com/metaplex/lib/modules/nfts/operations/FindNftsByCandyMachineOnChainOperationHandler.kt
@@ -2,6 +2,8 @@ package com.metaplex.lib.modules.nfts.operations
 
 import com.metaplex.lib.Metaplex
 import com.metaplex.lib.drivers.solana.Connection
+import com.metaplex.lib.modules.candymachines.models.CandyMachine
+import com.metaplex.lib.modules.candymachinesv2.models.CandyMachineV2
 import com.metaplex.lib.modules.nfts.models.NFT
 import com.metaplex.lib.shared.*
 import com.solana.core.PublicKey
@@ -10,10 +12,9 @@ import kotlinx.coroutines.Dispatchers
 
 data class FindNftsByCandyMachineInput(
     val candyMachine : PublicKey,
-    val version : UInt?,
+    val version : Int?,
 )
 
-val candyMachineId = PublicKey("cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ")
 class FindNftsByCandyMachineOnChainOperationHandler(override val connection: Connection,
                                                     override val dispatcher: CoroutineDispatcher = Dispatchers.IO)
     : OperationHandler<FindNftsByCandyMachineInput, List<NFT?>> {
@@ -21,16 +22,16 @@ class FindNftsByCandyMachineOnChainOperationHandler(override val connection: Con
     constructor(metaplex: Metaplex) : this(metaplex.connection)
 
     override suspend fun handle(input: FindNftsByCandyMachineInput): Result<List<NFT?>> {
-        val cmAddress = if((input.version ?: 2) == 2){
-            val pdaSeeds = listOf(
+        val cmAddress = if(input.version == 3){
+            PublicKey.findProgramAddress(listOf(
                 "candy_machine".toByteArray(),
-                candyMachineId.toByteArray(),
-            )
-            val pdaAddres = PublicKey.findProgramAddress(
-                pdaSeeds,
-                candyMachineId
-            )
-            pdaAddres.address
+                input.candyMachine.toByteArray(),
+            ), PublicKey(CandyMachine.PROGRAM_ADDRESS)).address
+        } else if(input.version == 2){
+            PublicKey.findProgramAddress(listOf(
+                CandyMachineV2.PROGRAM_NAME.toByteArray(),
+                input.candyMachine.toByteArray()
+            ), PublicKey(CandyMachineV2.PROGRAM_ADDRESS)).address
         } else input.candyMachine
 
         return FindNftsByCreatorOnChainOperationHandler(connection, dispatcher)
